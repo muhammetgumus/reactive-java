e798de2939a683c83bcb66e2f2df49811f207f02
Muhammet Gumus <muhammetgms95@gmail.com>
2022-08-02 21:15:40 +0300

    reactive stream operations and tests are added

--- src/test/java/com/muhammetgumus/ReactiveOperationsTest.java
+++ src/test/java/com/muhammetgumus/ReactiveOperationsTest.java
@@ -10,4 +10,39 @@ public class ReactiveOperationsTest extends TestCase {
     public void testFluxGenerator() {
         StepVerifier.create(ReactiveOperations.fluxGenerator()).expectNext("default").verifyComplete();
     }
+
+    @Test
+    public void concatExample() {
+        StepVerifier.create(ReactiveOperations.concatExample()).expectNextCount(6).verifyComplete();
+    }
+
+    @Test
+    public void concatWithExample() {
+        StepVerifier.create(ReactiveOperations.concatWithExample()).expectNext("A", "B", "C", "D").verifyComplete();
+    }
+
+    @Test
+    public void mergeExample() {
+        StepVerifier.create(ReactiveOperations.mergeExample()).expectNext("A", "D", "B", "C", "E", "F").verifyComplete();
+    }
+
+    @Test
+    public void mergeWithExample() {
+        StepVerifier.create(ReactiveOperations.mergeWithExample()).expectNext("A", "D", "B", "C", "E", "F").verifyComplete();
+    }
+
+    @Test
+    public void mergeWithMonoExample() {
+        StepVerifier.create(ReactiveOperations.mergeWithMonoExample()).expectNext("A", "B").verifyComplete();
+    }
+
+    @Test
+    public void mergeSequantialExample() {
+        StepVerifier.create(ReactiveOperations.mergeSequantialExample()).expectNext("A", "S", "D", "Q", "W", "E").verifyComplete();
+    }
+
+    @Test
+    public void zipExample() {
+        StepVerifier.create(ReactiveOperations.zipExample()).expectNext("AQ", "SW", "DE").verifyComplete();
+    }
 }
