b11c5b76d3012b7df9093d7bfae1048cffd0e33b
Muhammet Gumus <muhammetgms95@gmail.com>
2022-08-04 22:38:11 +0300

    new exception handling examples and tests are added

--- src/main/java/com/muhammetgumus/ReactiveExceptionHandling.java
+++ src/main/java/com/muhammetgumus/ReactiveExceptionHandling.java
@@ -11,15 +11,59 @@ public class ReactiveExceptionHandling {
         return Flux.just("Person1", "Person2", "Person3");
     }
 
-    /**
-     * onErrorReturn function's return value will be merge to the end of the flux
-     * @return Flux<String>
-     */
     public static Flux<String> onErrorReturnExample() {
         return fluxGenerator()
                 .concatWith(Flux.error(new IllegalStateException("Exception occured")))
                 .onErrorReturn("Person4")
                 .log();
     }
+
+    public static Flux<String> onErrorResumeExample() {
+        var recoveryFlux = Flux.just("Person4", "Person5");
+        return fluxGenerator()
+                .concatWith(Flux.error(new IllegalStateException("Error occured")))
+                .onErrorResume(e -> {
+                    System.out.println("Error is : " + e);
+                    return recoveryFlux;
+                }).log();
+    }
+
+    public static Flux<String> onErrorContinueExample() {
+        //You should use onErrorContinue() if you want to emit the remaining elements even the error is occurs
+        return fluxGenerator()
+                .map((person) -> {
+                    if ("Person2".equals(person)) {
+                        throw new IllegalStateException("Error : This person is not suitable");
+                    }
+                    return person.toUpperCase();
+                }).onErrorContinue((err, person) -> {
+                    System.out.println("Error details : " + err);
+                    System.out.println(person + " -> is not suitable in here");
+                }).log();
+
+    }
+
+    public static Flux<String> onErrorMapExample() {
+        return fluxGenerator().map((person) -> {
+                    if ("Person2".equals(person)) {
+                        throw new IllegalStateException("Error occured and be careful this error will be changed in future !!");
+                    }
+                    return person;
+                }).concatWith(Flux.just("Person4"))
+                .onErrorMap(err -> {
+                    return new IllegalAccessException(err.getClass() + " error type is the new expected type error BE CAREFUL! ");
+                }).log();
+    }
+
+    public static Flux<String> doOnErrorExample() {
+        //doOnError does not recover the remained parts it just catches error and then propagates it. It likes basic try-catch
+        return fluxGenerator()
+                .concatWith(Flux.error(new Exception("This is test exception")))
+                .doOnError((err)->{
+                    System.out.println("Error occurred : While catching error in here you could add your logic here");
+                })
+                .log();
+    }
+
 
 }
--- src/test/java/com/muhammetgumus/ReactiveExceptionHandlingTest.java
+++ src/test/java/com/muhammetgumus/ReactiveExceptionHandlingTest.java
@@ -8,8 +8,40 @@ public class ReactiveExceptionHandlingTest extends TestCase {
 
     @Test
     public void testOnErrorReturnExample() {
+        //onErrorReturn returns single value so that just "Person4" concat to the successfully emitted items
         StepVerifier.create(ReactiveExceptionHandling.onErrorReturnExample())
-                .expectNext("Person1","Person2","Person3","Person4")
+                .expectNext("Person1", "Person2", "Person3", "Person4")
                 .verifyComplete();
+    }
+
+    @Test
+    public void testOnErrorResumeExample() {
+        StepVerifier.create(ReactiveExceptionHandling.onErrorResumeExample())
+                .expectNext("Person1", "Person2", "Person3", "Person4", "Person5")
+                .verifyComplete();
+    }
+
+    @Test
+    public void testOnErrorContinueExample() {
+        StepVerifier.create(ReactiveExceptionHandling.onErrorContinueExample())
+                .expectNext("PERSON1", "PERSON3")
+                .verifyComplete();
+    }
+
+    @Test
+    public void testOnErrorMapExample() {
+        //onErrorMap converts the error but cut the process after error !!
+        StepVerifier.create(ReactiveExceptionHandling.onErrorMapExample())
+                .expectNext("Person1")
+                .expectError(IllegalAccessException.class)
+                .verify();
+    }
+
+    @Test
+    public void doOnErrorExample() {
+        StepVerifier.create(ReactiveExceptionHandling.doOnErrorExample())
+                .expectNext("Person1","Person2","Person3")
+                .expectError(Exception.class)
+                .verify();
     }
 }
