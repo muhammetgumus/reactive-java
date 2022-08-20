36a3b2acea79b263ae1b7773f17781a9949fdbd9
Muhammet Gumus <muhammetgms95@gmail.com>
2022-08-20 21:28:52 +0300

    BackpressureMechanism example added

---
+++ src/main/java/com/muhammetgumus/BackpressureMechanism.java
@@ -0,0 +1,46 @@
+package com.muhammetgumus;
+
+import org.reactivestreams.Subscription;
+import reactor.core.publisher.BaseSubscriber;
+import reactor.core.publisher.Flux;
+
+public class BackpressureMechanism {
+    public static void main(String[] args) {
+        introductionToBackpressure().subscribe(new BaseSubscriber<Integer>() {
+            @Override
+            protected void hookOnSubscribe(Subscription subscription) {
+                request(5);
+            }
+
+            @Override
+            protected void hookOnNext(Integer value) {
+                System.out.println("Next value is : " + value);
+                if (value % 5 == 0){
+                    System.out.println("\n*** New request sending to publisher! ***\n");
+                    request(5);
+                }
+            }
+
+            @Override
+            protected void hookOnComplete() {
+                System.out.println("Process completed");
+            }
+
+            @Override
+            protected void hookOnError(Throwable throwable) {
+                System.out.println("Error occurred!");
+            }
+
+            @Override
+            protected void hookOnCancel() {
+                System.out.println("Process is canceled!");
+                cancel();
+            }
+        });
+    }
+
+    public static Flux<Integer> introductionToBackpressure() {
+        return Flux.range(1, 20)
+                .log();
+    }
+}
