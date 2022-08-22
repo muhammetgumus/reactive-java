6f9cc34fcd46b7873fc7d21ecba73888dfcfd000
Muhammet Gumus <muhammetgms95@gmail.com>
2022-08-22 19:59:02 +0300

    Backpressure drop functionality added

--- src/main/java/com/muhammetgumus/BackpressureMechanism.java
+++ src/main/java/com/muhammetgumus/BackpressureMechanism.java
@@ -6,37 +6,43 @@ import reactor.core.publisher.Flux;
 
 public class BackpressureMechanism {
     public static void main(String[] args) {
-        introductionToBackpressure().subscribe(new BaseSubscriber<Integer>() {
-            @Override
-            protected void hookOnSubscribe(Subscription subscription) {
-                request(5);
-            }
+        introductionToBackpressure()
+                .onBackpressureDrop(item -> {
+                    //After request number "n" element will be dropped!
+                    System.out.println("Item dropped : " + item);
+                })
+                .subscribe(new BaseSubscriber<Integer>() {
+                    @Override
+                    protected void hookOnSubscribe(Subscription subscription) {
+                        request(5);
+                    }
 
-            @Override
-            protected void hookOnNext(Integer value) {
-                System.out.println("Next value is : " + value);
-                if (value % 5 == 0){
-                    System.out.println("\n*** New request sending to publisher! ***\n");
-                    request(5);
-                }
-            }
+                    @Override
+                    protected void hookOnNext(Integer value) {
+                        System.out.println("Next value is : " + value);
+                        //below commented for testing Backpressure Drop functionality
+                        /*if (value % 5 == 0) {
+                            System.out.println("\n*** New request sending to publisher! ***\n");
+                            request(5);
+                        }*/
+                    }
 
-            @Override
-            protected void hookOnComplete() {
-                System.out.println("Process completed");
-            }
+                    @Override
+                    protected void hookOnComplete() {
+                        System.out.println("Process completed");
+                    }
 
-            @Override
-            protected void hookOnError(Throwable throwable) {
-                System.out.println("Error occurred!");
-            }
+                    @Override
+                    protected void hookOnError(Throwable throwable) {
+                        System.out.println("Error occurred!");
+                    }
 
-            @Override
-            protected void hookOnCancel() {
-                System.out.println("Process is canceled!");
-                cancel();
-            }
-        });
+                    @Override
+                    protected void hookOnCancel() {
+                        System.out.println("Process is canceled!");
+
+                    }
+                });
     }
 
     public static Flux<Integer> introductionToBackpressure() {
