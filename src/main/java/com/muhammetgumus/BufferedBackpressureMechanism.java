6703c9b56d8e822673c6cbf3e747ac5bf79a875b
Muhammet Gumus <muhammetgms95@gmail.com>
2022-08-22 20:27:46 +0300

    Buffered backpressure mechanism example added

---
+++ src/main/java/com/muhammetgumus/BufferedBackpressureMechanism.java
@@ -0,0 +1,52 @@
+package com.muhammetgumus;
+
+import org.reactivestreams.Subscription;
+import reactor.core.publisher.BaseSubscriber;
+import reactor.core.publisher.Flux;
+
+public class BufferedBackpressureMechanism {
+    public static void main(String[] args) {
+        fluxGenerator()
+                .onBackpressureBuffer(10, lastItem -> {
+                    //After cancel operation 10 items will be buffered
+                    System.out.println("Last item is : " + lastItem);
+                })
+                .subscribe(new BaseSubscriber<Integer>() {
+                    @Override
+                    protected void hookOnSubscribe(Subscription subscription) {
+                        request(1);
+                    }
+
+                    @Override
+                    protected void hookOnNext(Integer value) {
+                        System.out.println("Next value is : " + value);
+                        if (value < 50) {
+                            request(1);
+                        } else {
+                            hookOnCancel();
+                        }
+                    }
+
+                    @Override
+                    protected void hookOnComplete() {
+                        System.out.println("Process completed");
+                    }
+
+                    @Override
+                    protected void hookOnError(Throwable throwable) {
+                        System.out.println("Error occurred!");
+                    }
+
+                    @Override
+                    protected void hookOnCancel() {
+                        System.out.println("Process is canceled!");
+
+                    }
+                });
+    }
+
+    public static Flux<Integer> fluxGenerator() {
+        return Flux.range(1, 100)
+                .log();
+    }
+}
